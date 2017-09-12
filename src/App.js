7377b161375ab902b7895de55aefbb14387b1f9a
Martin Chung <mwchung24@gmail.com>
2017-09-12 15:09:32 -0400

    Rendering the google maps API

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react';
 import './App.css';
 
+const google = window.google;
+
 class App extends Component {
+  // constructor(props) {
+  //   super(props);
+  // }
+
+  componentDidMount() {
+    const mapOptions = {
+      center: {lat: 40.7128, lng: -74.0059},
+      zoom: 15,
+    };
+    this.map = new google.maps.Map(this.mapNode, mapOptions);
+  }
+
+
 
   render() {
     return (
-      <div>
-
+      <div id='map-container' ref='map'>
+        <div className='googlemaps' ref={map => this.mapNode = map}></div>
       </div>
     );
   }
